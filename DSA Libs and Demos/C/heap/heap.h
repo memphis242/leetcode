6796c9115332b1965e56480eeba45d5c02ab342f
memphis242 <abdullah.almosalami.eng@gmail.com>
2025-04-13 10:23:07 -0500

    :memo: WIP: heap: Better naming at API

--- DSA Libs and Demos/C/heap/heap.h
+++ DSA Libs and Demos/C/heap/heap.h
@@ -5,6 +5,7 @@
 // Opaque type for a user to hold a handle to a heap to pass to the API
 struct Heap_S;
 
-struct Heap_S * HeapInit( int (* cmp)(const void * a, const void * b), size_t size_of_member );
+struct Heap_S * HeapInit( int (* cmp)(const void * a, const void * b),
+                          size_t element_size );
 // bool HeapifyArray( const void * array, size_t length, size_t size_of_member, int (* comp)(const void * a, const void * b) );
-// bool InsertIntoHeap( const void * element );
+// bool HeapInsert( const void * element );
