591e63e1dfe26b150676c03dc571810edb56a138
Abdullah Almosalami <AlmosalamiAbdullahI@JohnDeere.com>
2025-04-13 19:51:41 -0500

    :memo: WIP: :railway_car: vector: Implemented constructor

--- DSA Libs and Demos/C/vector/vector.c
+++ DSA Libs and Demos/C/vector/vector.c
@@ -18,12 +18,20 @@
 #include <stdint.h>
 
 /* Local Macro Definitions */
-#define MAX_VECTOR_LENGTH  1000000 // 1 million
+
+// Enforce a maximum length to help prevent extreme memory requests
+#define TENTATIVE_MAX_VEC_LEN 100000 // 100,000
+#if(SIZE_MAX < TENTATIVE_MAX_VEC_LEN)
+   #define MAX_VECTOR_LENGTH  SIZE_MAX
+#else
+   #define MAX_VECTOR_LENGTH  TENTATIVE_MAX_VEC_LEN
+#endif
 
 /* Local Datatypes */
 
 struct Vector_S
 {
+   void * vector;
    size_t element_size;
    uint32_t len;
 };
@@ -31,5 +39,32 @@ struct Vector_S
 /* Private Function Prototypes */
 
 /* Public API Implementations */
+
+// TODO: Support custom allocators passed in
+struct Vector_S * VectorInit( size_t element_size, uint32_t initial_length )
+{
+   // Early return op
+   // Invalid inputs
+   if ( (element_size == 0) ||
+        (initial_length > MAX_VECTOR_LENGTH) )
+   {
+      // TODO: Vector constructor exception → assert()?
+      return NULL;
+   }
+
+   struct Vector_S * NewVec = (struct Vector_S *)malloc( sizeof(struct Vector_S) );
+   NewVec->element_size = element_size;
+   NewVec->len = initial_length;
+   if ( 0 == initial_length )
+   {
+      NewVec->vector = NULL;
+   }
+   else
+   {
+      NewVec->vector = malloc( (size_t)(element_size * initial_length) );
+   }
+
+   return NewVec;
+}
 
 /* Private Function Implementations */
