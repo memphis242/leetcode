0263da1b5e0c4a0c87ff81901dc2844926531151
Abdullah Almosalami <AlmosalamiAbdullahI@JohnDeere.com>
2025-04-13 21:50:30 -0500

    :memo: WIP: :railway_car: vector: free & insert implm

--- DSA Libs and Demos/C/vector/vector.c
+++ DSA Libs and Demos/C/vector/vector.c
@@ -16,8 +16,19 @@
 #include <string.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <assert.h>
+
+// TODO: Realloc usage is a security vulnerability because data left behind from
+//       realloc moving the data rather than growing/shrinking could be sensitive
+//       and somehow exploited elsewhere. In this situation, I think you'd want
+//       provide a custom realloc that handles this situation.
 
 /* Local Macro Definitions */
+// Constants
+#define EXPANSION_FACTOR   (2.0f)
+
+// Function-like
+#define IS_EMPTY(self)     ( 0 == (self)->len )
 
 // Enforce a maximum length to help prevent extreme memory requests
 #define TENTATIVE_MAX_VEC_LEN 100000 // 100,000
@@ -33,43 +44,152 @@
 #endif
 
 /* Local Datatypes */
-
 struct Vector_S
 {
-   void * vector;
+   void * arr;
    size_t element_size;
    uint32_t len;
+   uint32_t capacity;
 };
 
 /* Private Function Prototypes */
+static bool LocalVectorExpand( struct Vector_S * self );
 
 /* Public API Implementations */
 
-// TODO: Support custom allocators passed in
-struct Vector_S * VectorInit( size_t element_size, uint32_t initial_length )
+struct Vector_S * VectorInit( size_t element_size, uint32_t initial_capacity )
 {
    // Early return op
    // Invalid inputs
    if ( (element_size == 0) ||
-        (initial_length > MAX_VECTOR_LENGTH) )
+        (initial_capacity > MAX_VECTOR_LENGTH) )
    {
       // TODO: Vector constructor exception → assert()?
       return NULL;
    }
 
    struct Vector_S * NewVec = (struct Vector_S *)malloc( sizeof(struct Vector_S) );
-   NewVec->element_size = element_size;
-   NewVec->len = initial_length;
-   if ( 0 == initial_length )
+   if ( 0 == initial_capacity )
    {
-      NewVec->vector = NULL;
+      NewVec->arr = NULL;
    }
    else
    {
-      NewVec->vector = malloc( (size_t)(element_size * initial_length) );
+      NewVec->arr = malloc( (size_t)(element_size * initial_capacity) );
    }
+   // If malloc failed to allocate space for the array...
+   if ( (initial_capacity > 0) && (NULL == NewVec->arr) )
+   {
+      // TODO: Throw exception to inform user...
+      NewVec->len = 0;
+   }
+   else
+   {
+      NewVec->len = initial_capacity;
+   }
+   NewVec->element_size = element_size;
 
    return NewVec;
 }
 
+void VectorFree( struct Vector_S * self )
+{
+   if ( self != NULL )
+   {
+      free(self->arr);
+   }
+   free(self);
+}
+
+bool VectorInsert( struct Vector_S * self, const void * restrict element )
+{
+   // Assertion on an internal paradox
+   assert( self->len <= self->capacity );
+
+   // Early return op
+   // Invalid inputs
+   if ( (NULL == self) || (NULL == element) )
+   {
+      // TODO: Throw exception
+      return false;
+   }
+
+   bool ret_val = true;
+
+   // Ensure there's space
+   bool successfully_expanded = true;
+   if ( self->len == self->capacity )
+   {
+      successfully_expanded = LocalVectorExpand(self);
+   }
+
+   if ( successfully_expanded )
+   {
+      void * insertion_spot = (unsigned char *)self->arr + (self->element_size * self->len);
+      memcpy( insertion_spot, element, self->element_size );
+      self->len++;
+   }
+   else
+   {
+      ret_val = false;
+   }
+
+   return ret_val;
+}
+
 /* Private Function Implementations */
+
+static bool LocalVectorExpand( struct Vector_S * self )
+{
+   // Since this is locally defined, I will destructively assert at any invalid inputs
+   assert( (self != NULL) && (self->element_size != 0) );
+
+   bool ret_val = true;
+
+   // If empty, create a single-element array
+   if ( IS_EMPTY(self) )
+   {
+      self->arr = malloc( self->element_size );
+      if ( NULL == self->arr )
+      {
+         ret_val = false;
+         self->capacity = 0;
+      }
+      else
+      {
+         self->capacity = 1;
+      }
+      self->len = 0;
+   }
+   else
+   {
+      // Assert if we somehow have a NULL arr when the vector was determined
+      // to be non-empty...
+      assert( self->arr != NULL );
+      uint32_t new_capacity;
+
+      if ( (uint32_t)(self->capacity * EXPANSION_FACTOR) <= MAX_VECTOR_LENGTH )
+      {
+         new_capacity = (uint32_t)(self->capacity * EXPANSION_FACTOR);
+      }
+      else
+      {
+         new_capacity = MAX_VECTOR_LENGTH;
+      }
+
+      void * new_ptr = realloc( self->arr, (self->element_size * new_capacity) );
+      if ( NULL == new_ptr )
+      {
+         // TODO: Throw exception
+         // TODO: Is the original pointer still usable?
+         ret_val = false;
+      }
+      else
+      {
+         self->arr = new_ptr;
+         self->capacity = new_capacity;
+      }
+   }
+
+   return ret_val;
+}
