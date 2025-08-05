af9435abb19da9fc1d8af19f88c201f686856aad
Abdullah Almosalami <abdullah.almosalami.eng@gmail.com>
2025-08-05 02:18:46 -0500

    :brain: 1. Two Sum : C++ Solution
    Ngl, feelin' proud of this solution. This problem is marked as an easy
    but I definitely was not expecting a map to show up here. LeetCode
    online will say I took 3h for this, but I just opened the problem before
    cooking, eating, and watching an hour-long video, and I'm stupid sleep
    rn. So, I actually took around a ½ hour of first thinking through
    possible moving window solutions before landing at the observations I
    made (see comment above method) and how I might exploit a "list" for
    this, which then turned into a map.

---
+++ 1 Two Sum/twosum.cpp
@@ -0,0 +1,38 @@
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+
+    // Observations To Take Advantage Of:
+    // Given any index's number, you know what its counterpart needs to be.
+    // Also, after going through a pair, there's no need to redo the same
+    // pair again.
+    // Solution Idea:
+    // As you go through each index, you build a list of the counterparts.
+    // Then, with each successive index, you check if the index is in the
+    // list. The list should be a heap so that insertions are logn, and lookups
+    // are logn.
+    vector<int> twoSum(vector<int>& nums, int target) {
+        if ( nums.size() <= 1 )
+            return vector<int>();
+
+        size_t idx1 = 0, idx2 = 0;
+        unordered_map<int, size_t> counterpart_list; // keys are counterparts, values are original idx
+        counterpart_list[target - nums[0]] = 0;
+        for ( size_t i = 1; i < nums.size(); i++ )
+        {
+            if ( counterpart_list.find(nums[i]) != counterpart_list.end() )
+            {
+                idx1 = counterpart_list.at(nums[i]);
+                idx2 = i;
+                break;
+            }
+            counterpart_list[target - nums[i]] = i;
+        }
+
+        return vector<int>{(int)idx1, (int)idx2};
+    }
+};
