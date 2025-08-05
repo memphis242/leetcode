204d9d39e77184e6f95223675f0a954216bb9b6a
Abdullah Almosalami <abdullah.almosalami.eng@gmail.com>
2025-08-05 12:46:40 -0500

    :brain: `3 Longest Substring Nonrepeatin` C++ : sliding window + set (best answer for now)
    Still not great. Only beast 34.34%.

--- 3 Longest Substring Without Repeating Characters/longest_substring_norepeat.cpp
+++ 3 Longest Substring Without Repeating Characters/longest_substring_norepeat.cpp
@@ -10,26 +10,30 @@ public:
     int lengthOfLongestSubstring(string s) {
         if ( s.length() <= 1 )
             return s.length();
-
-        size_t max_len = 0;
-        list<unordered_set<char>> substr_list;
-
-        for ( char c : s ) {
-            for ( auto it = substr_list.begin(); it != substr_list.end(); ) {
-                auto [_, inserted] = it->insert(c);
-                if ( !inserted ) {
-                    max_len = (it->size() > max_len) ? it->size() : max_len;
-                    it = substr_list.erase(it);
-                }
-                else {
-                    it++;
-                }
-            }
-            substr_list.emplace_back(unordered_set<char>{c});
+        else if ( s.length() == 2 ) {
+            if ( s[0] == s[1] )
+                return 1;
+            else
+                return 2;
         }
-        size_t longest_remaining_len = substr_list.front().size();
-        max_len = (longest_remaining_len > max_len) ? longest_remaining_len : max_len;
 
-        return static_cast<int>(max_len);
+        size_t max = 1;
+        unordered_set<char> strset;
+        strset.insert(s[0]);
+        strset.insert(s[1]);
+        size_t left = 0, right = 1;
+        while ( (right + 1) < s.length() ) {
+            auto [_,inserted] = strset.insert(s[right + 1]);
+            if ( !inserted ) {
+                max = (max >= strset.size()) ? max : strset.size();
+                strset.erase(s[left]);
+                left++;
+            } else {
+                right++;
+            }
+        }
+
+        max = (max >= strset.size()) ? max : strset.size();
+        return max;
     }
 };
