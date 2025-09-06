171e45d7d7189b720f310fb530a58c2ad71dc7af
memphis242 <abdullah.almosalami.eng@gmail.com>
2025-09-06 15:17:55 -0500

    :mag_right: WIP: Initial UTs for bare min fcn pass

--- dsa/C++/topK/test.cpp
+++ dsa/C++/topK/test.cpp
@@ -5,23 +5,31 @@
 TEST(Test_Top2, EmptyRange)
 {
    TopK<int, 2> topk(std::vector<int>{});
-   EXPECT_EQ(true, false);
+   auto topVals = topk.topK();
+   EXPECT_FALSE(topVals[0].has_value());
+   EXPECT_FALSE(topVals[1].has_value());
 }
 
 TEST(Test_Top2, SmallRange_Sorted)
 {
    TopK<int, 2> topk( std::vector<int>({1, 2, 3, 4, 5}) );
-   EXPECT_EQ(true, false);
+   auto topVals = topk.topK();
+   EXPECT_EQ(*topVals[0], 4);
+   EXPECT_EQ(*topVals[1], 5);
 }
 
 TEST(Test_Top2, SmallRange_Unsorted)
 {
    TopK<int, 2> topk( std::vector<int>({5, 2, 1, 3, 6}) );
-   EXPECT_EQ(true, false);
+   auto topVals = topk.topK();
+   EXPECT_EQ(*topVals[0], 5);
+   EXPECT_EQ(*topVals[1], 6);
 }
 
 TEST(Test_Top2, SmallRange_RepeatingNum)
 {
-
-   EXPECT_EQ(true, false);
+   TopK<int, 2> topk( std::vector<int>({5, 5, 5, 5, 5}) );
+   auto topVals = topk.topK();
+   EXPECT_EQ(*topVals[0], 5);
+   EXPECT_EQ(*topVals[1], 5);
 }
